bb7d669856efe7d02144f6527e845d9319208a49
Nmoser101 <nmoser@iastate.edu>
2025-03-12 20:22:29 -0500

    added space

---
+++ Main_Code/Main_Code.ino
@@ -0,0 +1,46 @@
+// *
+//This code is designed by the Iowa State Robotics for use on a RMD-X motor
+//*
+
+//Libraries for using the CAN board
+#include <SPI.h>       
+#include <mcp2515.h>
+
+struct can_frame canMsg;
+MCP2515 mcp2515(10); //sets reading to pin 10
+int position = 1; // sets a variable to be read later on, 1 is starting position
+
+
+
+
+
+void setup() 
+//This sets up the Can Sheild and check to make sure it is working
+{
+    while (!Serial);
+  Serial.begin(9600); // set to serial baud monitor rate 
+  
+  mcp2515.reset();
+  mcp2515.setBitrate(CAN_500KBPS,MCP_16MHZ); //set the CAN bus speed to 500KBPS and the frequency of the crystal oscillator to 16 MHz
+  mcp2515.setNormalMode();
+
+  Serial.print("Setup done\n\n");
+}
+
+//Sets up so that Can's 8 bit is proccessed correctly
+void printserial(unsigned long ID, unsigned char buf[8]) {
+  // converts buf[8] to a character string 
+    Serial.print(ID, HEX);
+    Serial.print(": ");
+    for (int i = 0; i < 8; i++)
+    {
+      Serial.print(buf[i], HEX);
+      Serial.print("  ");
+    }
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
